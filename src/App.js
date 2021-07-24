40b4cf537a44ad0939edce08bbeac102582a63d2
Jaffer Ali <jafferali150@gmail.com>
2021-07-25 03:56:33 +0530

    Basic support skeleton implementation for Fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Fetch from "./components/fetch";
 
 function App() {
+  const [ip, setIP] = useState(0);
+  useEffect(() => {
+    async function fetchit() {
+      // var raw = await Fetch();
+      setIP(await Fetch());
+      // console.log(raw);
+    }
+
+    fetchit();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <h6>{ip}</h6>
     </div>
   );
 }
