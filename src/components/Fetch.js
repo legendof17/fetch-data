454d321fd0a2dfbf96538c6fc31e63ccd4504771
Jaffer Ali <jafferali150@gmail.com>
2021-07-25 20:08:32 +0530

    updated api mechanism

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const Fetch = async () => {
-  const raw = await axios.get(
-    "http://demo.ip-api.com/json/?fields=66842623&lang=en"
-    // "./fakeresponse.json"
-  );
-  const asCode = raw["data"]["as"].split(" ")[0];
-  Object.assign(raw["data"], { asCode: asCode });
-  // console.log(asCode);
+  const raw = await axios.get("https://ipapi.co/json");
+  const raw2 = await axios.get("https://extreme-ip-lookup.com/json");
+  // const raw = await axios.get("./fakeresponse.json");
+  Object.assign(raw["data"], {
+    ipName: raw2["data"]["ipName"],
+    ipType: raw2["data"]["ipType"],
+    latitude: raw2["data"]["lat"],
+    longitude: raw2["data"]["lon"],
+  });
   console.log(raw["data"]);
 
   return raw["data"];
